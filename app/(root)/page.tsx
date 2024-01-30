513582d004f608954a0f64128e0d6bc7b2acbe1f
void <95859137+voiiddxx@users.noreply.github.com>
2024-01-30 18:02:41 +0530

    changes(root/portfolio/create) added ui and react hook form using shadcn and zod

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,21 +2,15 @@
 import { Button } from "@/components/ui/button";
 import './Main.css'
 import connectToDatabase from "@/lib/database/mongodb";
+import Hero from "@/components/shared/home/Hero";
 export default function Home() {
 
   const db = ()=>{
     connectToDatabase();
   }
   return (
-    <div className="mainhai">
-      <div className="min-h-screen w-full flex flex-col justify-start items-center pt-28">
-      <h1 className="bg-gradient-to-tl from-white to-gray-500 text-transparent bg-clip-text font-bold text-6xl text-center leading-normal ">Unleash your protfolio with<br /> <span className="bg-gradient-to-tr from-purple-600 to-orange-600 text-transparent bg-clip-text" >With SwiftFolio</span></h1>
-      <p className="text-gray-400 text-center mt-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Cum voluptatem sapiente <br /> consectetur officia natus eveniet delectus reiciendis sequi aperiam cupiditate.</p>
-      <div className="mt-5 flex gap-3" >
-        <Button className="bg-white text-black">Create Now</Button>
-        <Button className="bg-transparent border-[0.5px] border-blue-500 text-blue-300">Give a Star on Github</Button>
-      </div>
-    </div>
-    </div>
+    <>
+    <Hero/>
+    </>
   );
 }
