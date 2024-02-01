d385571d3924421b02cd223e9b07d5762ba1a122
void <95859137+voiiddxx@users.noreply.github.com>
2024-02-01 12:50:23 +0530

    resolving the clerk path error

--- app/(auth)/layout.tsx
+++
@@ -1,9 +0,0 @@
-const Layout = ({children} : {children : React.ReactNode}) => {
-     return (
-        <div className="min-h-screen w-full flex items-center justify-center bg-gray-950">
-            {children}
-        </div>
-     )
-}
-
-export default Layout
