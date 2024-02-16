2404c644789e867b0035e4e89e6aa5795eb211f5
void <95859137+voiiddxx@users.noreply.github.com>
2024-02-16 15:25:20 +0530

    Work: -- Action -- : impleneted get action for work using clerkid

--- lib/actions/work.action.ts
+++ lib/actions/work.action.ts
@@ -19,6 +19,22 @@ export const AddWorkExperince =  async ({work , clerkId} : AddWorkParams) => {
          await isWork[0].save();
         }
     } catch (error) {
+        throw new Error(error as string);
+    }
+}
+
+
+export const getWorkExperinceAsPerclerkId = async (clerkId: string) => {
+    try {
+        await connectToDatabase();
+        const userWorkExperince = await Work.find({clerkId: clerkId});
+        if(!userWorkExperince){
+            return JSON.parse(JSON.stringify({message: " User work not found"}));
+        }
+
+        return JSON.parse(JSON.stringify(userWorkExperince));
         
+    } catch (error) {
+        throw new Error(error as string);
     }
 }
