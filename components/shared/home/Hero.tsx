445c2d95a1612efd352c18187265c464a3f17f11
void <95859137+voiiddxx@users.noreply.github.com>
2024-03-02 18:45:38 +0530

    CHnaging -- Ui for work exp -- college - school -

--- components/shared/home/Hero.tsx
+++ components/shared/home/Hero.tsx
@@ -10,18 +10,18 @@ const Hero = () => {
 
   const router = useRouter();
   return (
-   <div className="bg-white  md:bg-HeroPageTheme bg-MobileMailBgImage bg-center bg-cover" >
+   <div className="bg-white  md:bg-HeroPageTheme" >
     <Header/>
     <div className=" px-4 md:min-h-screen w-full flex flex-col items-center justify-start pb-24">
       <h1 className="text-zinc-900 md:text-5xl text-3xl text-center font-semibold md:mt-44 mt-28" >Unleash Your Creativity</h1>
       <h1 className="text-zinc-900 md:text-5xl text-3xl text-center font-semibold  mt-3 " >Boom Your Resume With the </h1>
       <h1 className="text-indigo-700 md:text-5xl text-3xl text-center font-semibold  mt-3 " >Help of SwiftFolio</h1>
       <p className="text-zinc-700 md:mr-96 md:ml-96 px-4 text-center mt-5" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae necessitatibus dignissimos, praesentium quos dolores dolorum impedit dicta magnam doloribus! </p>
-      <div className="flex gap-2 mt-6 mx-10" >
-        <Button className="bg-transparent border-indigo-800 hover:border-white hover:bg-white text-white" variant="outline"><p className="text-indigo-800  flex items-center" >
+      <div className="flex gap-2 mt-6 mx-16" >
+        <Button className="bg-transparent border-indigo-800 hover:border-white hover:bg-white text-white" variant="outline" ><p className="text-indigo-800  flex items-center" >
           <Zap className="mr-1 text-indigo-800"  size={16} />
-          Register on SwiftFolio</p></Button>
-        <Button className="bg-indigo-700 text-white hover:bg-zinc-900"  > <Ghost className="mr-1" color="white" size={16} /> Create Your Portfolio</Button>
+          Register Now</p></Button>
+        <Button className="bg-indigo-700 text-white hover:bg-zinc-900"  > <Ghost className="mr-1" color="white" size={16} /> Create  Portfolio</Button>
       </div>
 
     </div>
--- components/themecomponents/darkpremium/DarkQualification.tsx
+++ components/themecomponents/darkpremium/DarkQualification.tsx
@@ -5,7 +5,7 @@ import { deleteWorkById } from '@/lib/actions/work.action'
 import { ICollege } from '@/lib/database/models/education.model'
 import { ISchool } from '@/lib/database/models/school.model'
 import { IWork } from '@/lib/database/models/work.model'
-import { Edit, Trash } from 'lucide-react'
+import { BookOpen, Building2, Edit, GraduationCap, Trash } from 'lucide-react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -48,144 +48,54 @@ const DarkQualification = ({userwork , userSchool , userCollege , isAdmin} : edu
   
   
   return (
-    <div className='w-full  bg-heroBgImage pt-14'>
+    <div className='w-full  bg-heroBgImage pt-14 md:px-32'>
       {/* schooling section  */}
-
     {
-       userSchool[0].length   < 1 ? <div></div> :  <>
-      <div className='pl-10 mt-4 mb-10 md:pl-60'>
-    <h1 className='text-yellow-300 text-xl' >My Education</h1>
-    <p className='mt-2 text-zinc-500' >All of my work experinece have been mentioned below</p>
-  </div> 
-{
-    userSchool[0].school.map((curr : ISchool) => {
-      return  <div className='w-full flex pl-10 md:pl-52 gap-2 items-start'>
-      {/* //border line  */}
-      <div className='flex flex-col'>
-          <Image className='h-24 w-32' src="/college.svg" height={500} width={500} alt='schoo and college icons'/>
-          <div className='ml-14'>
-              <div className='md:h-40 h-20 w-[3px] rounded-md mt-[-14px] bg-gradient-to-b from-green-300 to-zinc-900'>
-                  
-              </div>
+      userSchool[0].school.length < 1 ? <div></div> : <>
+      <div>
+        <h1 className='text-green-300 text-2xl font-medium' >Education</h1>
+        <p className='text-zinc-500 mt-2' >Schooling and my college are mentioned below</p>
+      </div>
+     <div className='mt-9' >
+     {
+        userSchool[0].school.map((curr : ISchool)=>{
+          return <div className='flex gap-4' >
+           <div className='h-16 w-20 bg-zinc-700 bg-opacity-25 rounded-md flex justify-center items-center' >
+            <Building2 className='text-green-400' size={30} />
+           </div>
+           <div>
+            <h1 className='text-green-300 text-lg' >Kiran Public School <span className='text-sm text-zinc-400 ml-2' >  (2021-2025)</span> </h1>
+            <p className='text-zinc-100 mt-1' >12th Non Medical</p>
+           <div className='w-[350px] h-16 mt-1 overflow-hidden' >
+           <p  className='text-zinc-500 text-[12px] ' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, officiis laudantium. Commodi, error illo.</p>
+           </div>
+           </div>
           </div>
-      </div>
-      {/* schooling informaton */}
-      <div className='md:pr-52 pr-10' >
-          <p className='text-zinc-500 font-normal text-sm' >{curr.finalYear}</p>
-          <h1 className='text-lg font-semibold text-zinc-200 mt-2'>{curr.schoolClass}</h1>
-          <p className='text-green-300 font-normal text-sm mt-2' >{curr.schoolName}</p>
-          <p className='text-zinc-600 font-normal md:mr-80 mt-2'>{curr.extraDetail}</p>
-
-          {
-        isAdmin && ( <div className='flex gap-4 mt-4' >
-       
-        <Trash onClick={()=>{
-            handleSchoolDelte(curr._id);
-        }} className='text-red-800 cursor-pointer' size={16} />
-      </div>)
-       }
-      </div>
-
-    </div>
-    })
-}
-
-</>
-
-      }
-
-
-
-{
-    userCollege[0].college.map((curr : ICollege) => {
-      return  <div className='w-full flex pl-10 md:pl-52 gap-2 items-start '>
-      {/* //border line  */}
-      <div className='flex flex-col'>
-          <Image className='h-24 w-32' src="/college.svg" height={500} width={500} alt='schoo and college icons'/>
-          <div className='ml-14'>
-              <div className='md:h-40 h-20 w-[3px] rounded-md mt-[-14px] bg-gradient-to-b from-green-300 to-zinc-900'>
-                  
-              </div>
-          </div>
-      </div>
-      {/* schooling informaton */}
-      <div className='md:pr-52 pr-10' >
-          <p className='text-zinc-500 font-normal text-sm' >{curr.batchStartDate} - {curr.batchEndDate}</p>
-          <h1 className='text-lg font-semibold text-zinc-200 mt-2'>{curr.degree}</h1>
-          <p className='text-green-300 font-normal text-sm mt-2' >{curr.instituteName}</p>
-          <p className='text-zinc-600 font-normal md:mr-80 mt-2 text-sm md:text-md'></p>
-
-          {
-        isAdmin && ( <div className='flex gap-4 mt-4' >
-       
-        <Trash onClick={()=>{
-            handleSchoolDelte(curr._id);
-        }} className='text-red-800 cursor-pointer' size={16} />
-      </div>)
-       }
-      </div>
-
-    </div>
-    })
-}
-
-
-   
-         
-      {/* work experince section  */}
-
-      
-
-       {
-       userwork.length   < 1 ? <div></div> :  <> <div className='md:pl-60 pl-10 mt-4 mb-10 w-full bg-heroBgImage'>
-        <h1 className='text-green-400 text-xl' >Work Experience</h1>
-        <p className='mt-2 text-zinc-500' >All of my work experinece have been mentioned below</p>
-      </div>
-      {
-        userwork[0].work.map((curr : IWork , index : number) => {
-          return  <div className='w-full flex md:pl-52 pl-10 gap-2 items-start'>
-          {/* //border line  */}
-          <div className='flex flex-col'>
-              <Image className='h-24 w-34' src="/job2.svg" height={500} width={500} alt='schoo and college icons'/>
-              <div className='ml-8'>
-
-              {
-              index == userwork[0].work.length -1 ?  <div className='h-40 w-[3px] rounded-md mt-[-14px] bg-transparent'>
-                  
-              </div> :  <div className='h-40 w-[3px] rounded-md mt-[-14px] bg-gradient-to-b from-blue-300 to-zinc-900'>
-                  
-              </div>
-            }
-
-              </div>
-          </div>
-          {/* schooling informaton */}
-          <div className='md:pr-52 pr-10' >
-          <p className='text-zinc-500 font-normal text-sm' >{curr.startDate} - {curr.endDate}  </p>
-          <h1 className='text-lg font-semibold text-zinc-200  mt-2'>{curr.role}</h1>
-          <p className='text-blue-300 font-normal text-sm mt-2' >{curr.companyName}</p>
-          
-          <p className='text-zinc-600 font-normal md:mr-80 mt-2 text-[12px] md:text-md'>{curr.contribution}</p>
-          {
-        isAdmin && ( <div className='flex gap-4 mt-4' >
-      
-        <Trash onClick={()=>{
-            handleWorkDelete(curr._id);
-        }} className='text-red-800 cursor-pointer' size={16} />
-      </div>)
-       }
-      </div>
-  
-        </div> 
         })
-
       }
+     </div>
+
+
+     <div className='mt-9' >
+     {
+        userSchool[0].school.map((curr : ISchool)=>{
+          return <div className='flex gap-4' >
+           <div className='h-16 w-20 bg-zinc-700 bg-opacity-25 rounded-md flex justify-center items-center' >
+            <BookOpen className='text-orange-300' size={30} />
+           </div>
+           <div>
+            <h1 className='text-orange-300 text-lg' >Kiran Public School <span className='text-sm text-zinc-400 ml-2' >  (2021-2025)</span> </h1>
+            <p className='text-zinc-100 mt-1' >12th Non Medical</p>
+           <div className='w-[350px] h-16 mt-1 overflow-hidden' >
+           <p  className='text-zinc-500 text-[12px] ' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut, officiis laudantium. Commodi, error illo.</p>
+           </div>
+           </div>
+          </div>
+        })
+      }
+     </div>
       </>
-      
-      } 
-
-
-    
+    }
       
 
     
@@ -202,6 +112,15 @@ export default DarkQualification
 
 
 
+
+// {
+//   isAdmin && ( <div className='flex gap-4 mt-4' >
+
+//   <Trash onClick={()=>{
+//       handleWorkDelete(curr._id);
+//   }} className='text-red-800 cursor-pointer' size={16} />
+// </div>)
+//  }
 
 
 // {
