4711a708c257661dc2e855a544ac9b66406f7b3e
void <95859137+voiiddxx@users.noreply.github.com>
2024-03-05 22:33:03 +0530

    Change text of home page

--- components/shared/home/Hero.tsx
+++ components/shared/home/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
       <h1 className="text-zinc-900 md:text-5xl text-3xl text-center font-semibold md:mt-44 mt-28" >Unleash Your Creativity</h1>
       <h1 className="text-zinc-900 md:text-5xl text-3xl text-center font-semibold  mt-3 " >Boom Your Portfolio With the </h1>
       <h1 className="text-indigo-700 md:text-5xl text-3xl text-center font-semibold  mt-3 " >Help of SwiftFolio</h1>
-      <p className="text-zinc-700 md:mr-96 md:ml-96 px-4 text-center mt-5" >Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae necessitatibus dignissimos, praesentium quos dolores dolorum impedit dicta magnam doloribus! </p>
+      <p className="text-zinc-700 md:mr-96 md:ml-96 px-4 text-center mt-5" >Swiftfolio is a platform where any individual can create thier portfolio with just one click, and boost their portfolio with creative design templates  </p>
       <div className="flex gap-2 mt-6 mx-16" >
       <Link href='/sign-in' >  <Button className="bg-transparent border-indigo-800 hover:border-white hover:bg-white text-white" variant="outline" ><p className="text-indigo-800  flex items-center" >
           <Zap className="mr-1 text-indigo-800"  size={16} />
