f26b6f5193ce75a5ddb5ab5e3d957c42d9be1826
void <95859137+voiiddxx@users.noreply.github.com>
2024-03-07 21:45:48 +0530

    redisgned header -- tranformed header sticky

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image"
 import { Button } from "../ui/button"
 import logo from '../../public/logo.svg'
-import { Dices, Flame, User } from "lucide-react"
+import { ArrowRight, Code2, Dices, Flame, LayoutTemplate, ListVideo, Play, Sparkles, User, View } from "lucide-react"
 import Link from "next/link"
 import {
   NavigationMenu,
@@ -21,17 +21,17 @@ const Header = () => {
   return (
     <>
     <div className="h-10 w-full flex justify-center items-center bg-violet-700" >
-    <Flame className="text-white mr-1" size={15} />
+    <Flame className="text-white mr-1" size={10} />
       <p className="text-[12px] text-white" >
         Free access to all template to our first 50 coustomer</p>
     </div>
-    <div className=" sticky top-0 w-full h-20 py-4 md:px-32 flex items-center justify-between border-b border-zinc-200 bg-white text-white" > 
+    <div className=" sticky top-0 w-full h-20 py-4 md:px-32 flex items-center justify-between  border-zinc-200 bg-white text-white" > 
     <div>
       <Image className="h-40 w-40"  src="/logo.svg" height={800} width={800} alt="logo" />
      
     </div>
-    <div>
-      <h1>Hello</h1>
+    <div className="flex" >
+   
     <NavigationMenu>
   <NavigationMenuList>
     <NavigationMenuItem>
@@ -39,24 +39,69 @@ const Header = () => {
         <p className="text-black font-medium tect-lg" >Getting Started</p>
         </NavigationMenuTrigger>
       <NavigationMenuContent>
-        <div className="h-[300px] w-[500px] bg-white shadow-[0_8px_30px_rgb(0,0,0,0.12)]" >
-          <div className="h-20 bg-red-300 m-2" >
-            <div className="m-2" ></div>
+        <div className="pb-3 w-[500px] bg-white shadow-[0_8px_30px_rgb(0,0,0,0.12)]" >
+         {/* row 1 */}
+         <div className="h-20  rounded border-b m-2 hover:bg-zinc-100 cursor-pointer" >
+            <div className="m-2 h-full w-full flex gap-4 items-center" >
+              <div className="h-14 w-14 bg-violet-500 bg-opacity-10 rounded-sm flex justify-center items-center border-[1px] border-violet-200" >
+                <ListVideo className="text-violet-700" />
+              </div>
+             <div>
+             <h1 className="text-zinc-900 font-medium" >SwiftFolio Tour</h1>
+              <p className="text-sm font-medium text-zinc-500" >An overview of the swiftfolio for what we offer</p>
+             </div>
+            </div>
+          </div>
+         {/* row 2 */}
+         <div className="h-20  rounded border-b m-2 hover:bg-zinc-100 cursor-pointer" >
+            <div className="m-2 h-full w-full flex gap-4 items-center" >
+              <div className="h-14 w-14 bg-violet-500 bg-opacity-10 rounded-sm flex justify-center items-center border-[1px] border-violet-200" >
+                <LayoutTemplate className="text-violet-700" />
+              </div>
+             <div>
+             <h1 className="text-zinc-900 font-medium" >Minimal Design</h1>
+              <p className="text-sm font-medium text-zinc-500" >Get Large varity of creative and minimal designs </p>
+             </div>
+            </div>
+          </div>
+         {/* row3 */}
+         <div className="h-20  rounded border-b m-2 hover:bg-zinc-100 cursor-pointer" >
+            <div className="m-2 h-full w-full flex gap-4 items-center" >
+              <div className="h-14 w-14 bg-violet-500 bg-opacity-10 rounded-sm flex justify-center items-center border-[1px] border-violet-200" >
+                <Code2 className="text-violet-700" />
+              </div>
+             <div>
+             <h1 className="text-zinc-900 font-medium" >Customization</h1>
+              <p className="text-sm font-medium text-zinc-500" >An overview of the swiftfolio for what we offer</p>
+             </div>
+            </div>
+          </div>
+         {/* row 4 */}
+         <div className="h-20  rounded border-b m-2 hover:bg-zinc-100 cursor-pointer" >
+            <div className="m-2 h-full w-full flex gap-4 items-center" >
+              <div className="h-14 w-14 bg-violet-500 bg-opacity-10 rounded-sm flex justify-center items-center border-[1px] border-violet-200" >
+                <Sparkles className="text-violet-700" />
+              </div>
+             <div>
+             <h1 className="text-zinc-900 font-medium" >Swiftfolio AI</h1>
+              <p className="text-sm font-medium text-zinc-500" >Genreate cool and creative details about project , about , anything</p>
+             </div>
+            </div>
           </div>
         </div>
       </NavigationMenuContent>
     </NavigationMenuItem>
     <NavigationMenuItem>
       <NavigationMenuTrigger>
-        <p className="text-black font-medium tect-lg" >Getting Started</p>
+        <p className="text-black font-medium tect-lg" >Pricing</p>
         </NavigationMenuTrigger>
       <NavigationMenuContent>
-        <NavigationMenuLink>Link</NavigationMenuLink>
+        {/* <NavigationMenuLink>Link</NavigationMenuLink> */}
       </NavigationMenuContent>
     </NavigationMenuItem>
     <NavigationMenuItem>
       <NavigationMenuTrigger>
-        <p className="text-black font-medium tect-lg" >Getting Started</p>
+        <p className="text-black font-medium tect-lg" >Workflow</p>
         </NavigationMenuTrigger>
       <NavigationMenuContent>
        
@@ -64,16 +109,11 @@ const Header = () => {
     </NavigationMenuItem>
   </NavigationMenuList>
 </NavigationMenu>
-
     </div>
-      
-      {/* <Image className="md:h-16 h-12" src={`/logo.svg`} height={500} width={200} alt="logo"/>
-      <div className="md:mr-24 mr-8" >
-        <div className="px-3 py-3 rounded-sm bg-violet-700  flex justify-center items-center" >
-         <Link href="/template" >
-         <p className="flex gap-2 items-center text-[12px] " ><Dices size={20} /> Templates</p></Link>
-        </div>
-      </div> */}
+    <div className="flex gap-2" >
+    <Button size={"lg"} variant={"outline"} className="text-zinc-800"  >Login Now</Button>
+    <Button className="bg-violet-700" size={"lg"} >Get Started For Free <ArrowRight className="ml-1"  strokeWidth={1.5} color="white" /> </Button>
+    </div>
     </div>
     </>
   )
