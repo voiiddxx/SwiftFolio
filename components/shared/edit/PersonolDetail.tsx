4189e97fca089c7903899adc9df825f419280946
void <95859137+voiiddxx@users.noreply.github.com>
2024-03-11 22:41:54 +0530

    added more component for the persenol edit

--- components/shared/edit/PersonolDetail.tsx
+++ components/shared/edit/PersonolDetail.tsx
@@ -1,5 +1,6 @@
 import { Button } from '@/components/ui/button'
 import { Input } from '@/components/ui/input'
+import { Textarea } from '@/components/ui/textarea'
 import { Mail, User } from 'lucide-react'
 import React from 'react'
 
@@ -13,7 +14,8 @@ const PersonolDetail = () => {
                 </div>
 
                 {/* PERSENOL DATA EDIT COMPONENTS */}
-                <div className='min-h-52 w-full  flex px-12 ' >
+                <div className='min-h-52 w-full flex px-12 gap-4' >
+                    {/* 1ST COLUMN FOR DATA OF EMAIL AND NAME UPDATION */}
                     <div className='min-h-40 py-5 w-1/2 rounded-lg   border-[1px] border-zinc-300 bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] mt-5 px-4' >
                         <div className=''  >
                             <h1 className='text-xl font-medium' >Primary Info</h1>
@@ -44,8 +46,77 @@ const PersonolDetail = () => {
                         </div>
                     
                     </div>
+                    {/* 1ST COLUMN FOR DATA OF EMAIL AND NAME UPDATION END */}
+
+
+                    {/* 2ST COLUMN FOR DATA OF IMAGE RESUME AND THE CASUAL AVATAR */}
+                    <div className='min-h-40 py-5 w-1/2 rounded-lg   border-[1px] border-zinc-300 bg-white shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] mt-5 px-4' >
+                        <div className=''  >
+                            <h1 className='text-xl font-medium' >Your Assets</h1>
+                            <p className='text-sm font-normal mt-1 text-zinc-600' >Update your assets</p>
+                        </div>
+
+                        
+                        <div className='mt-5' >
+                            <p className='text-zinc-500 font-normal text-sm mt-4 mb-1' >Your CV</p>
+                           <div className='flex gap-2 items-center ' >
+                           <div className='h-10  w-12 bg-slate-100 rounded flex justify-center items-center' >
+                                <User strokeWidth={1.5} size={17} />
+                            </div>
+                            <Input type='file' />
+                           </div>
+                           
+                            <p className='text-zinc-500 font-normal text-sm mt-4 mb-1' >Your Avatar</p>
+                           <div className='flex gap-2 items-center ' >
+                           <div className='h-10  w-12 bg-slate-100 rounded flex justify-center items-center' >
+                                <Mail strokeWidth={1.5} size={17} />
+                            </div>
+                            <Input type='file' />
+                           </div>
+                            <p className='text-zinc-500 font-normal text-sm mt-4 mb-1' >Your Casual Image</p>
+                           <div className='flex gap-2 items-center ' >
+                           <div className='h-10  w-12 bg-slate-100 rounded flex justify-center items-center' >
+                                <Mail strokeWidth={1.5} size={17} />
+                            </div>
+                            <Input type='file' />
+                           </div>
+
+                           <Button className='w-full mt-5' >Submit</Button>
+                           
+                            
+                        </div>
+                    
+                    </div>
+                    {/* 2nd column for upadtion of emage , resume  and the casual END */}
                 </div>
 
+
+        {/* CAPTION AND ABOUT US EDIT SECTION */}
+
+        <div className='w-full min-h-40 px-12 py-6 rounded-md' >
+            <div className='w-full  bg-white rounded-md shadow-[rgba(50,_50,_105,_0.15)_0px_2px_5px_0px,_rgba(0,_0,_0,_0.05)_0px_1px_1px_0px] border-[1px] border-zinc-300 px-5 py-4' >
+                {/* heading */}
+                <div>
+            <h1>Additnols</h1>
+            <p className='text-sm text-zinc-600' >Update as per requirement</p>
+                </div>
+
+                {/* flex for about and heading */}
+
+                <div className='w-full h-full  flex gap-4' >
+                    <div className='w-1/2 mt-4' >
+                        <p className='mb-1' >Heading</p>
+                        <Textarea className='h-40 w-full' />
+                    </div>
+                    <div className='w-1/2 mt-4' >
+                        <p className='mb-1' >Heading</p>
+                        <Textarea className='h-40 w-full' />
+                    </div>
+                </div>
+
+                <Button className='w-full mt-4' >Submit </Button>
+            </div>
+        </div>
                
 
             </div>
